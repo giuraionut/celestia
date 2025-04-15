55b656d9f8029e35e3ae409ba6bc51ee737f2690
giuraionut98 <giuraionut98@gmail.com>
2025-04-15 23:35:30 +0300

    prisma app client

--- prisma/app/generated/prisma/client/edge.js
+++ prisma/app/generated/prisma/client/edge.js
@@ -261,7 +261,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../../../.env",
     "schemaEnvPath": "../../../../../.env"
   },
   "relativePath": "../../../..",
